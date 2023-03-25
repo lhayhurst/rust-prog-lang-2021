564f61b07f137cc75103dc451fb45c8bf09f5d08
Lyle Hayhurst <wlhayhurst@gmail.com>
2023-03-25 15:23:16 -0700

    chpt4 first draft

---
+++ chpt4/ownership/src/lib.rs
@@ -0,0 +1,34 @@
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_strcat() {
+        let mut s = String::from("Hello");
+        s.push_str(", world!");
+        assert_eq!(s, "Hello, world!");
+    }
+
+    #[test]
+    fn test_move_and_copy_semantics() {
+        // integers don't act like references (pg 68)
+        // because they implement the Copy trait
+        // other types that implement the Copy trait, are
+        // booleans, floats, characters, and tuples
+        let x = 5;
+        let mut y = x;
+
+        y = y + 1;
+        assert_eq!(x, 5);
+        assert_eq!(y, 6);
+
+        // but strings do
+        let s1 = String::from("Hello");
+
+        let mut s2 = s1.clone();
+        s2.push_str(", world!");
+
+        assert_eq!(s2, "Hello, world!");
+        assert_eq!(s1, "Hello");
+    }
+
+}
