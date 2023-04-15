82fea02d6bde69dad4417cfa7ef7cad24c77a2cc
Lyle Hayhurst <wlhayhurst@gmail.com>
2023-04-15 12:42:15 -0700

    added string tests

--- chpt8/string/src/lib.rs
+++ chpt8/string/src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+#[test]
+fn test_string_create() {
+    let mut s = String::new();
+    assert_eq!(0, s.len());
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[test]
+fn test_make_string_from_literal() {
+    let data = "initial contents";
+    let s = data.to_string();
+    assert_eq!(s, data);
 }
+
+#[test]
+fn test_updating_a_string() {
+    let mut s = String::from("foo");
+    s.push_str("bar");
+    assert_eq!(s, "foobar");
+}
+
+#[test]
+fn test_append_character_to_a_string() {
+    let mut s = String::from("lo");
+    s.push('l');
+    assert_eq!(s, "lol");
+}
+
+#[test]
+fn test_str_concat() {
+    let s1 = String::from("Hello, ");
+    let s2 = String::from("world!");
+    let s3 = s1 + &s2;
+    assert_eq!(s3, "Hello, world!");
+}
+
+#[test]
+fn test_str_concat_via_format() {
+    let s1 = String::from("tic");
+    let s2 = String::from("tac");
+    let s3 = String::from("toe");
+
+    let s4 = format!("{s1}-{s2}-{s3}");
+    assert_eq!(s4, "tic-tac-toe");
+}
+
