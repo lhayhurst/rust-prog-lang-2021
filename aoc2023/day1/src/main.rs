281894dea6b9f8c8a99c2a3d7129a4ad8623d760
Lyle Hayhurst <wlhayhurst@gmail.com>
2023-12-03 14:36:53 +0000

    fixed refactoring bug

--- aoc2023/day1/src/main.rs
+++ aoc2023/day1/src/main.rs
@@ -1,7 +1,7 @@
 use std::fs::File;
 use std::io::{self, BufRead};
-use prob1_and_2::get_calibration_value;
-use prob1_and_2::get_numbers_from_string;
+use day1::get_calibration_value;
+use day1::get_numbers_from_string;
 
 pub fn read_file_lines(file_path: &str) -> Result<Vec<String>, io::Error> {
     let file = File::open(file_path)?;
